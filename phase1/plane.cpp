ad0c0d3c7b2d4b77f3dcf3afe05dd2103817d04a
FabioBaiao <a75662@alunos.uminho.pt>
2017-02-23 20:39:13 +0000

    Changed pseudocode for plane simplest case

--- phase1/plane.cpp
+++ phase1/plane.cpp
@@ -17,15 +17,20 @@ void plane (float l, FILE* f){
 	/*
 		print the following to f
 
-	//number of vertices
+	vertices:
+	A -> l 0 l
+	B -> l 0 -l
+	C -> -l 0 -l
+	D -> -l 0 l
+	//number of vertices for the triangles (2 triangles each with 3 vertices)
 	6
 	//triangle #1
-	l 0 l (vertex A)
-	l 0 -l (vertex B)
-	-l 0 l (vertex D)
+	vertex A
+	vertex B
+	vertex D
 	//triangle #2
-	l 0 -l (vertex B)
-	-l 0 -l (vertex C)
-	-l 0 l (vertex D)
+	vertex B
+	vertex C
+	vertex D
 	*/
 }
