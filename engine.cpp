adcdf5af83aeb7ac627c29e62ee6925c47cd8bd3
João Pereira <joaopereira_19@hotmail.com>
2017-03-01 00:02:28 +0000

    started to parse xml files for config

--- engine.cpp
+++ engine.cpp
@@ -1,7 +1,10 @@
 #define TIXML_USE_STL 
-#include "tinyxml/tinystr.h"
 #include "tinyxml/tinyxml.h"
+#include "point.h"
+#include <vector>
+#include <string>
 
+//We assume that the .xml and .3d files passed are correct.
 int main(int argc, char** argv){
 	if(argc != 2){
 		std::cout << "The command invocation must be in the form 'engine filename'\n";
@@ -13,5 +16,18 @@ int main(int argc, char** argv){
 	if(!loadOkay){
 		std::cout << "Error to load file " << argv[1] << ".\n";
 		return -1;
+	}
+
+	std::vector<point> points{};
+	TiXmlHandle docHandle(&doc);
+	TiXmlElement* model = doc.FirstChild("scene")->FirstChild("model")->ToElement();
+	for(; model; model=model->NextSiblingElement()){
+		std::string filenameFromXml, filename; 
+		int r = model->QueryStringAttribute("file", &filenameFromXml);
+		if(r == TIXML_SUCCESS){
+			filename = filenameFromXml.substr(1, filenameFromXml.length()-2);
+			//ifstream f = open(filename); 
+			std::cout << filename;
+		}
 	}
 }
