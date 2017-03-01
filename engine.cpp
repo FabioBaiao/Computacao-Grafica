9ab5afa28009ff12874fe48a27f3edea53f49d7e
João Pereira <joaopereira_19@hotmail.com>
2017-03-01 09:13:38 +0000

    parse of the filename added

--- engine.cpp
+++ engine.cpp
@@ -1,33 +1,45 @@
 #define TIXML_USE_STL 
+#define BUFF_SIZE 128
 #include "tinyxml/tinyxml.h"
 #include "point.h"
 #include <vector>
+#include <fstream>
 #include <string>
 
+using namespace std;
+
 //We assume that the .xml and .3d files passed are correct.
+string parseFileName(string filenameFromXml){
+	// for some reason, the '\"' symbol uses 3bytes
+	return filenameFromXml.substr(3, filenameFromXml.length()-6); 
+}
+
 int main(int argc, char** argv){
 	if(argc != 2){
-		std::cout << "The command invocation must be in the form 'engine filename'\n";
+		cout << "The command invocation must be in the form 'engine filename'\n";
 		return -1;
 	}
 
 	TiXmlDocument doc(argv[1]);
 	bool loadOkay = doc.LoadFile();
 	if(!loadOkay){
-		std::cout << "Error to load file " << argv[1] << ".\n";
+		cout << "Error to load file " << argv[1] << ".\n";
 		return -1;
 	}
 
-	std::vector<point> points{};
+	vector<point> points{};
 	TiXmlHandle docHandle(&doc);
 	TiXmlElement* model = doc.FirstChild("scene")->FirstChild("model")->ToElement();
 	for(; model; model=model->NextSiblingElement()){
-		std::string filenameFromXml, filename; 
-		int r = model->QueryStringAttribute("file", &filenameFromXml);
+		string filename; 
+		int r = model->QueryStringAttribute("file", &filename);
 		if(r == TIXML_SUCCESS){
-			filename = filenameFromXml.substr(1, filenameFromXml.length()-2);
-			//ifstream f = open(filename); 
-			std::cout << filename;
+			parseFileName(filename);
+			ifstream file;
+			file.open(filename);
+			int n_vertex; 
+			file >> n_vertex;
+			cout << n_vertex;
 		}
 	}
 }
