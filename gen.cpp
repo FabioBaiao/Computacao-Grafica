6117e8f61a4e4b445565e6c407735e7bc250ddf2
jlabm <a68646@alunos.uminho.pt>
2017-03-05 05:07:53 +0000

    Added validations, a usage message  and the functions for generating cones, frustums, cylinders and toruses to gen.cpp

--- gen.cpp
+++ gen.cpp
@@ -1,7 +1,279 @@
 #include <string>
 #include <iostream>
 
-std::string drawSphere(float radius, float slices, int stacks) {
+#include <stdlib.h>
+
+int boxGenerator(int argc, char *argv[]) {
+	int divisions;
+	float xDim, yDim, zDim;
+	ofstream outfile;
+
+	xDim = atof(argv[0]);
+	yDim = atof(argv[1]);
+	zDim = atof(argv[2]);
+	divisions = (argc == 5 ? atoi(argv[3]) : 1);
+
+	if(xDim <= 0.0f || yDim <= 0.0f || zDim <= 0.0f || divisions <= 0) {
+		fputs("Error: All parameters of the box must be positive numbers\n", stderr);
+		return 1;
+	}
+	
+	(argc == 4) ? outfile.open(argv[3]) : outfile.open(argv[4]));
+	if(!outfile.is_open()) {
+		perror("ofstream.open");
+		return 1;
+	}
+	outfile << box(xDim, yDim, zDim, divisions);
+	outfile.close();
+	return 0;
+}
+
+int coneGenerator(int argc, char *argv[]) {
+	float radius, height;
+	int slices, stacks;
+	ofstream outfile;
+
+	radius = atof(argv[0]);
+	height = atof(argv[1]);
+	slices = atoi(argv[2]);
+	stacks = atoi(argv[3]);
+	if(radius <= 0.0f || height <= 0.0f || slices <= 0 || stacks <= 0) {
+		fputs("Error: All parameters of the cone must be positive numbers\n", stderr);
+		return 1;
+	}
+	outfile.open(argv[4]);
+	if(!outfile.is_open()) {
+		perror("ofstream.open");
+		return 1;
+	}
+	outfile << cone(radius, height, slices, stacks);
+	outfile.close();
+	return 0;
+}
+
+// Cylinder and cone generators are very similar. Consider reusing code!
+int cylinderGenerator(int argc, char *argv[]) {
+	float radius, height;
+	int slices, stacks;
+	ofstream outfile;
+
+	radius = atof(argv[0]);
+	height = atof(argv[1]);
+	slices = atoi(argv[2]);
+	stacks = atoi(argv[3]);
+	if(radius <= 0.0f || height <= 0.0f || slices <= 0 || stacks <= 0) {
+		fputs("Error: All parameters of the cylinder must be positive numbers\n", stderr);
+		return 1;
+	}
+	outfile.open(argv[4]);
+	if(!outfile.is_open()) {
+		perror("ofstream.open");
+		return 1;
+	}
+	outfile << cylinder(radius, height, slices, stacks);
+	outfile.close();
+	return 0;
+}
+
+int ellipsoidGenerator(int argc, char *argv[]) {
+	float xRadius, yRadius, zRadius;
+	int slices, stacks;
+	ofstream outfile;
+
+	xRadius = atof(argv[0]);
+	yRadius = atof(argv[1]);
+	zRadius = atof(argv[2]);
+	slices = atoi(argv[3]);
+	stacks = atoi(argv[4]);
+	if(xRadius <= 0.0f || yRadius <= 0.0f || zRadius <= 0.0f || slices <= 0 || stacks <= 0) {
+		fputs("Error: All parameters of the cylinder must be positive numbers\n", stderr); // consider creating a macro for this kind of error message
+		return 1;
+	}
+	outfile.open(argv[5]);
+	if(!outfile.is_open()) {
+		perror("ofstream.open");
+		return 1;
+	}
+	outfile << ellipsoid(xRadius, yRadius, zRadius, slices, stacks);
+	outfile.close();
+	return 0;
+}
+
+int frustumGenerator(int argc, char *argv[]) {
+	float baseRadius, topRadius, height;
+	int slices, stacks;
+	ofstream outfile;
+
+	baseRadius = atof(argv[0]);
+	topRadius = atof(argv[1]);
+	height = atof(argv[2]);
+	slices = atoi(argv[3]);
+	stacks = atoi(argv[4]);
+	if(baseRadius < 0.0f || topRadius < 0.0f) {
+		fputs("Error: The base radius and the top radius must both be non-negative\n", stderr);
+		return 1;
+	}
+	if(height <= 0 || slices <= 0 || stacks <= 0) {
+		fputs("Error: Height, slices and stacks must all be positive\n", stderr);
+		return 1;
+	}
+	
+	outfile.open(argv[5]);
+	if(!outfile.is_open()) {
+		perror("ofstream.open");
+		return 1;
+	}
+	outfile << frustum(baseRadius, topRadius, height, slices, stacks);
+	outfile.close();
+	return 0;
+}
+
+int planeGenerator(int argc, char *argv[]) {
+	float xDim, yDim;
+	int divisions;
+	ofstream outfile;
+
+	xDim = atof(argv[0]);
+	yDim = atof(argv[1]);
+	divisions = (argc == 4 ? atoi(argv[2]) : 1);
+	if(xDim <= 0.0f || yDim <= 0.0f || divisions <= 0) {
+		fputs("Error: All parameters of the plane must be positive numbers\n", stderr);
+		return 1;
+	}
+	(argc == 3) ? outfile.open(argv[2]) : outfile.open(argv[3]);
+
+	if(!outfile.is_open()) {
+		perror("ofstream.open");
+		return 1;
+	}
+	outfile << plane(xDim, yDim, divisions);
+	outfile.close();
+	return 0;
+}
+
+int sphereGenerator(int argc, char *argv[]) {
+	float radius;
+	int slices, stacks;
+	ofstream outfile;
+
+	radius = atof(argv[0]);
+	slices = atoi(argv[1]);
+	stacks = atoi(argv[2]);
+	if(radius <= 0.0f || slices <= 0 || stacks <= 0) {
+		fputs("Error: All parameters of the sphere must be positive numbers\n", stderr);
+		return 1;
+	}
+
+	outfile.open(argc[3]);
+	if(!outfile.is_open()) {
+		perror("ofstream.open");
+		return 1;
+	}
+	outfile << sphere(radius, slices, stacks);
+	outfile.close();
+	return 0;
+}
+
+int torusGenerator(int argc, char *argv[]) {
+	float innerRadius, outerRadius;
+	int sides, rings;
+	ofstream outfile;
+
+	innerRadius = atof(argv[0]);
+	outerRadius = atof(argv[1]);
+	sides = atoi(argv[2]);
+	stacks = atoi(argv[3]);
+	if(innerRadius <= 0.0f || outerRadius <= 0.0f || sides <= 0 || rings <= 0) {
+		fputs("Error: All parameters of the torus must be positive numbers\n", stderr);
+		return 1;
+	}
+	outfile.open(argc[3]);
+	if(!outfile.is_open()) {
+		perror("ofstream.open");
+		return 1;
+	}
+	outfile << torus(innerRadius, outerRadius, sides, rings);
+	outfile.close();
+	return 0;
+}
+
+std::string frustum(float baseRadius, float topRadius, float height, int slices, int stacks) {
+	int i, j, nPoints = 0;
+	float *cosCache, *sinCache;
+	float dr, dh, alpha, dAlpha;
+	std::string ret = "";
+	std::ostringstream os;
+
+	cosCache = new float[slices+1];
+	sinCache = new float[slices+1];
+	alpha = 0.0f;
+	dAlpha = 2.0f * M_PI / slices;
+	for(i = 0; i < slices; i++) {
+		alpha = i * dAlpha;
+		cosCache[i] = cosf(alpha);
+		sinCache[i] = sinf(alpha);
+	}
+	cosCache[slices] = cosCache[0]; // cos(2 * M_PI) = cos(0)
+	sinCache[slices] = sinCache[0]; // sin(2 * M_PI) = sin(0)
+
+	if(baseRadius > 0.0f) {
+		for(i = 0; i < slices; i++) { // lower base
+			os << (baseRadius * sinCache[i]) << " 0.0 " << (baseRadius * cosCache[i]) << '\n';
+			os << "0.0 0.0 0.0\n";
+			os << (baseRadius * sinCache[i+1]) << " 0.0 " << (baseRadius * cosCache[i+1]) << '\n';
+			nPoints += 3;
+		}
+	}
+	if(topRadius > 0.0f) {
+		for(i = 0; i < slices; ++i) { // top base
+			os << (topRadius * sinCache[i]) << ' ' << height << ' ' << (topRadius * cosCache[i]) << '\n';
+			os << (topRadius * sinCache[i+1]) << ' ' << height << ' ' << (topRadius * cosCache[i+1]) << '\n';
+			os << "0.0 " << height << " 0.0\n";
+			nPoints += 3;
+		}
+	}
+	dr = (baseRadius - topRadius) / stacks;
+	dh = height / stacks;
+	// side of the frustum
+	for(i = 0; i < stacks; ++i) {
+		float rLow, rHigh;
+		float yLow, yHigh;
+
+		rLow = baseRadius - i * dr;
+		rHigh = rLow - dr;
+		yLow = i * dh;
+		yHigh = yLow + dh;
+		if(baseRadius > 0.0f) {
+			for(j = 0; j < slices; ++j) {
+				os << (rLow * sinCache[j]) << ' ' << yLow << ' ' << (rLow * cosCache[j]) << '\n';
+				os << (rHigh * sinCache[j+1]) << ' ' << yHigh << ' ' << (rHigh * cosCache[j+1]) << '\n';
+				os << (rHigh * sinCache[j]) << ' ' << yHigh << ' ' << (rHigh * cosCache[j]) << '\n';
+				nPoints += 3;
+			}
+		}
+		if(topRadius > 0.0f) {
+			for(j = 0; j < slices; ++j) {
+				os << (rLow * sinCache[j]) << ' ' << yLow << ' ' << (rLow * cosCache[j]) << '\n';
+				os << (rLow * sinCache[j+1]) << ' ' << yLow << ' ' << (rLow * cosCache[j+1]) << '\n';
+				os << (rHigh * sinCache[j+1]) << ' ' << yHigh << ' ' << (rHigh * cosCache[j+1]) << '\n';
+				nPoints += 3;
+			}
+		}
+	}
+	delete[] cosCache; delete[] sinCache;
+
+	return (std::toString(nPoints) + os.str());
+}
+
+std::string cone(float radius, float height, int slices, int stacks) {
+	return frustum(radiusm, 0.0f, height, slices, stacks);
+}
+
+std::string cylinder(float radius, float height, int slices, int stacks) {
+	return frustum(radius, radius, height, slices, stacks);
+}
+
+std::string sphere(float radius, float slices, int stacks) {
 
 	std:string ret = "";
 	int count = 0;
@@ -57,48 +329,109 @@ std::string drawSphere(float radius, float slices, int stacks) {
 	return std::to_string(count) + "\n" + ret; 
 }
 
-int main(int argc, char ** argv){
+std::string torus(float innerRadius, float outerRadius, int nsides, int nrings) {
+	int i, j, nPoints = 0;
+	float deltaPhi, deltaTheta;
+	float phi, theta, nextPhi, nextTheta;
+	float cosPhi, cosTheta, sinPhi, sinTheta;
+	float cosNextPhi, cosNextTheta, sinNextPhi, sinNextTheta;
+	std::string res = "";
+	std::ostringstream os;
+
+	deltaPhi = 2.0f * M_PI / nrings;
+	deltaTheta = 2.0f * M_PI / nsides;
+	for(i = 0; i < nrings; i++) {
+		phi = i * deltaPhi;
+		nextPhi = (i + 1) * deltaPhi;
+		cosPhi = cosf(phi);
+		sinPhi = sinf(phi);
+		cosNextPhi = cosf(nextPhi);
+		sinNextPhi = sinf(nextPhi);
+
+		for(j = 0; j < nsides; j++, nPoints += 6) {
+			float dXZ, nextDXZ;
+
+			theta = j * deltaTheta;
+			nextTheta = (j + 1) * deltaTheta;
+			cosTheta = cosf(theta);
+			sinTheta = sinf(theta);
+			cosNextTheta = cosf(nextTheta);
+			sinNextTheta = sinf(nextTheta);
+			dXZ = outerRadius + innerRadius * cosTheta;
+			nextDXZ = outerRadius + innerRadius * cosNextTheta;
+			
+			os << (dXZ * sinPhi) << ' ' << (innerRadius * sinTheta) << ' ' << (dXZ * cosPhi) << '\n';
+			os << (nextDXZ * sinNextPhi) << ' ' << (innerRadius * sinNextTheta) << ' ' << (nextDXZ * cosNextPhi) << '\n';
+			os << (nextDXZ * sinPhi) << ' ' << (innerRadius * sinNextTheta) << ' ' << (nextDXZ * cosPhi) << '\n';
+			
+			os << (dXZ * sinPhi) << ' ' << (innerRadius * sinTheta) << ' ' << (dXZ * cosPhi) << '\n';
+			os << (dXZ * sinNextPhi) << ' ' << (innerRadius * sinTheta) << ' ' << (dXZ * cosNextPhi) << '\n';
+			os << (nextDXZ * sinNextPhi) << ' ' << (innerRadius * sinNextTheta) << ' ' << (nextDXZ * cosNextPhi) << '\n';
+		}
+	}
+	return (std::to_string(nPoints) + os.str());
+}
+
+void usage(const char *programName, FILE *stream) {
+	fprintf(stream, "Usage: %s primitive parameters outfile\n\n", programName);
+	fputs("+-------------+-------------------------------------------+"
+		  "| primitive   | parameters                                |"
+		  "+-------------+-------------------------------------------+"
+		  "| box         | xDim yDim zDim [divisions]                |"
+		  "| cone        | radius height slices stacks               |"
+		  "| cylinder    | radius height slices stacks               |"
+		  "| ellipsoid   | xRadius yRadius zRadius slices stacks     |"
+		  "| frustum     | baseRadius topRadius height slices stacks |"
+		  "| plane       | xDim yDim [divisions]                     |"
+		  "| sphere      | radius slices stacks                      |"
+		  "| torus       | innerRadius outerRadius sides rings       |"
+		  "+-------------+-------------------------------------------+"
+		  , stream
+	);
+}
+
+int main(int argc, char *argv[]) {
+	int rval;
+
 	if(argc == 1){
-		print_help();
-		return 0;
+		usage(argv[0], stderr);
+		return 1;
 	} 
 
 	std::string primitive(argv[1]); 
 	std::vector<Point> points;
-	char* filename;
-
-	if(primitive == "plane" && argc == 5){
-		//validate arguments
-		ofstream file = open(argv[4]);
-		file << drawPlane(...);
-		close(file);
-	}
-	else if(primitive == "box" && argc == 7){
-		//validate arguments
-		ofstream file = open(argv[6]);
-		file << drawBox(...);
-		close(file);
-	}
-	else if(primitive == "sphere" && argc == 6){
-		//validate arguments
-		ofstream file = open(argv[5]);
-		file << drawSphere(atof(argv[2]), atof(argv[3]), atof(argv[4]));
-		close(file);
+	if(primitive == "box" && (argc == 6 || argc == 7)) {
+		rval = boxGenerator(argc - 2, &argv[2]);
 	}
 	else if(primitive == "cone" && argc == 7){
-		//validate arguments
-		ofstream file = open(argv[6]);
-		file << drawCone(...);
-		close(file);
+		rval = coneGenerator(argc - 2, &argv[2]);
 	}
-	else if(primitive == "torus" && argc == 10000){
-		//to-do if time available
+	else if(primitive == "cylinder" && argc == 7){
+		rval = cylinderGenerator(argc - 2, &argv[2]);
 	}
-	else if(primitive == "cilinder" && argc == 1000){
-		//to-do if time available
+	else if(primitive == "ellipsoid" && argc == 8) {
+		rval = ellipsoidGenerator(argc - 2, &argv[2]);
 	}
-	else{
-		print_help();
-		return -1;
+	else if(primitive == "frustum" && argc) {
+		rval = frustumGenerator(argc - 2, &argv[2]);
 	}
+	else if(primitive == "plane" && (argc == 5 || argc == 6)) {
+		rval = planeGenerator(argc - 2, &argv[2]);
+	}
+	else if(primitive == "sphere" && argc == 6){
+		rval = sphereGenerator(argc - 2, &argv[2]);
+	}
+	else if(primitive == "torus" && argc == 7){
+		rval = torusGenerator(argc - 2, &argv[2]);
+	}
+	else if(primitive == "--help") {
+		usage(argv[0], stdout);
+		rval = 0;
+	} else {
+		usage(argv[0], stderr);
+		rval = 1;
+	}
+	return rval;
 }
+
+
