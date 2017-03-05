494ab058aad0fb16d913398305833f081151e260
FabioBaiao <a75662@alunos.uminho.pt>
2017-03-05 14:35:09 +0000

    Started using stringstreams and deleted all unnecessary code

--- phase1/box.cpp
+++ phase1/box.cpp
@@ -1,383 +1,6 @@
-//AGAIN, SORRY FOR BAD ENGLISH
-
-/*
-	The final version is in the bottom
-	----------------------------------
-*/
-
-/*
-	This pseudocode is for the box without division numbers.
-	When adding the code for the case with division numbers, we can remove this one, and use the new with division number = 1
-*/
-
-void box (float x, float y, float z, FILE *f){
-	
-	/*
-		x, y and z must be greater than 0
-	*/
-
-	x = x/2;
-	y = y/2;
-	z = z/2;
-
-	/*
-
-	I'm assuming that the given coordinates are according to the OpenGL coordinate system
-	vertices:
-	A -> x -y z
-	B -> x y z
-	C -> -x y z
-	D -> -x -y z
-	E -> x -y -z
-	F -> x y -z
-	G -> -x y -z
-	H -> -x -y -z
-	//number of vertices for the triangles (6 faces each with 2 triangles each with 3 vertices)
-	36
-	//front face
-	//triangle #1
-	vertex A
-	vertex B
-	vertex D
-	//triangle #2
-	vertex B
-	vertex C
-	vertex D
-	//back face
-	//triangle #1
-	vertex E
-	vertex H
-	vertex F
-	//triangle #2
-	vertex F
-	vertex H
-	vertex G
-	//up face
-	//trangle #1
-	vertex B
-	vertex F
-	vertex C
-	//triangle #2
-	vertex C
-	vertex F
-	vertex G
-	//down face
-	//triangle #1
-	vertex A
-	vertex D
-	vertex E
-	//triangle #2
-	vertex D
-	vertex H
-	vertex E
-	//left face
-	//triangle #1
-	vertex C
-	vertex H
-	vertex D
-	//triangle #2
-	vertex C
-	vertex G
-	vertex H
-	//right face
-	//triangle #1
-	vertex A
-	vertex E
-	vertex B
-	//triangle #2
-	vertex B
-	vertex E
-	vertex F
-	*/
-}
-
-void box2 (float x, float y, float z, float nDiv){
-
-	/*
-		x, y, z and nDiv must be greater than 0.
-	*/
-
-	float deltaX = x / nDiv;
-	float deltaY = y / nDiv;
-	float deltaZ = z / nDiv;
-
-	float xi, yi, zi;
-
-	x = x/2;
-	y = y/2;
-	z = z/2;
-
-	/*
-		FRONT FACE
-
-		The xi, yi, zi are the initial value to print each rectangle (not each face).
-		Their starting value is the bottom right corner of the face to be printed.
-	*/
-	xi = x;
-	yi = -y;
-	zi = z;
-	/* 
-		The outer cycle iterates through each line of the face (down -> up).
-	*/
-	for (int i = 0; i < nDiv; i++){
-		/*
-			The inner cycle prints all rectangles of the same line in the face being print (from the rigth to the left).
-		*/
-		for (int j = 0; j < nDiv; j++){
-			/*
-				xi yi zi; //(starting vertex)
-				xi (yi+deltaY) zi; //(above vertex)
-				(xi-deltaX) (yi+deltaY) zi; //(left-up diagonal vertex)
-
-				xi yi zi; //(starting vertex)
-				(xi-deltaX) (yi+deltaY) zi; //(left-up diagonal vertex)
-				(xi-deltaX) yi zi; //(left vertex)
-			*/
-			xi -= deltaX; // the next iteration is at the left of the starting value.
-		}
-		/*
-			To start a new line, it's necessary to go back to the inital bottom right corner but one line up. 
-		*/
-		xi += nDiv * deltaX;
-		yi += deltaY;
-	}
-
-	/*
-		BACK FACE
-	*/
-	xi = -x;
-	yi = -y;
-	zi = -z;
-	/* 
-		The outer cycle iterates through each line of the face (down -> up).
-	*/
-	for (int i = 0; i < nDiv; i++){
-		/*
-			The inner cycle prints all rectangles of the same line in the face being print (from the rigth to the left).
-		*/
-		for (int j = 0; j < nDiv; j++){
-			/*
-				xi yi zi; //(starting vertex)
-				xi (yi+deltaY) zi; //(above vertex)
-				(xi+deltaX) (yi+deltaY) zi; //(left-up diagonal vertex)
-
-				xi yi zi; //(starting vertex)
-				(xi+deltaX) (yi+deltaY) zi; //(left-up diagonal vertex)
-				(xi+deltaX) yi zi; //(left vertex)
-			*/
-			xi += deltaX; // the next iteration is at the left of the starting value.
-		}
-		/*
-			To start a new line, it's necessary to go back to the inital bottom right corner but one line up. 
-		*/
-		xi -= nDiv * deltaX;
-		yi += deltaY;
-	}
-
-	/*
-		UP FACE
-	*/
-	xi = x;
-	yi = y;
-	zi = z;
-	/* 
-		The outer cycle iterates through each line of the face (down -> up).
-	*/
-	for (int i = 0; i < nDiv; i++){
-		/*
-			The inner cycle prints all rectangles of the same line in the face being print (from the rigth to the left).
-		*/
-		for (int j = 0; j < nDiv; j++){
-			/*
-				xi yi zi; //(starting vertex)
-				xi yi (zi-deltaZ); //(above vertex)
-				(xi-deltaX) yi (zi-deltaZ); //(left-up diagonal vertex)
-
-				xi yi zi; //(starting vertex)
-				(xi-deltaX) yi (zi-deltaZ); //(left-up diagonal vertex)
-				(xi-deltaX) yi zi; //(left vertex)
-			*/
-			xi -= deltaX; // the next iteration is at the left of the starting value.
-		}
-		/*
-			To start a new line, it's necessary to go back to the inital bottom right corner but one line up. 
-		*/
-		xi += nDiv * deltaX;
-		zi -= deltaZ;
-	}
-
-	/*
-		DOWN FACE
-	*/
-	xi = x;
-	yi = -y;
-	zi = -z;
-	/* 
-		The outer cycle iterates through each line of the face (down -> up).
-	*/
-	for (int i = 0; i < nDiv; i++){
-		/*
-			The inner cycle prints all rectangles of the same line in the face being print (from the rigth to the left).
-		*/
-		for (int j = 0; j < nDiv; j++){
-			/*
-				xi yi zi; //(starting vertex)
-				xi yi (zi+deltaZ); //(above vertex)
-				(xi-deltaX) yi (zi+deltaZ); //(left-up diagonal vertex)
-
-				xi yi zi; //(starting vertex)
-				(xi-deltaX) yi (zi+deltaZ); //(left-up diagonal vertex)
-				(xi-deltaX) yi zi; //(left vertex)
-			*/
-			xi -= deltaX; // the next iteration is at the left of the starting value.
-		}
-		/*
-			To start a new line, it's necessary to go back to the inital bottom right corner but one line up. 
-		*/
-		xi += nDiv * deltaX;
-		zi += deltaZ;
-	}
-
-	/*
-		RIGHT FACE
-	*/
-	xi = x;
-	yi = -y;
-	zi = -z;
-	/* 
-		The outer cycle iterates through each line of the face (down -> up).
-	*/
-	for (int i = 0; i < nDiv; i++){
-		/*
-			The inner cycle prints all rectangles of the same line in the face being print (from the rigth to the left).
-		*/
-		for (int j = 0; j < nDiv; j++){
-			/*
-				xi yi zi; //(starting vertex)
-				xi (yi+deltaY) zi; //(above vertex)
-				xi (yi+deltaY) (zi+deltaZ); //(left-up diagonal vertex)
-
-				xi yi zi; //(starting vertex)
-				xi (yi+deltaY) (zi+deltaZ); //(left-up diagonal vertex)
-				xi yi (zi+deltaZ); //(left vertex)
-			*/
-			zi += deltaZ; // the next iteration is at the left of the starting value.
-		}
-		/*
-			To start a new line, it's necessary to go back to the inital bottom right corner but one line up. 
-		*/
-		zi -= nDiv * deltaZ;
-		yi += deltaY;
-	}
-
-	/*
-		RIGHT FACE
-	*/
-	xi = -x;
-	yi = -y;
-	zi = z;
-	/* 
-		The outer cycle iterates through each line of the face (down -> up).
-	*/
-	for (int i = 0; i < nDiv; i++){
-		/*
-			The inner cycle prints all rectangles of the same line in the face being print (from the rigth to the left).
-		*/
-		for (int j = 0; j < nDiv; j++){
-			/*
-				xi yi zi; //(starting vertex)
-				xi (yi+deltaY) zi; //(above vertex)
-				xi (yi+deltaY) (zi-deltaZ); //(left-up diagonal vertex)
-
-				xi yi zi; //(starting vertex)
-				xi (yi+deltaY) (zi-deltaZ); //(left-up diagonal vertex)
-				xi yi (zi-deltaZ); //(left vertex)
-			*/
-			zi -= deltaZ; // the next iteration is at the left of the starting value.
-		}
-		/*
-			To start a new line, it's necessary to go back to the inital bottom right corner but one line up. 
-		*/
-		zi += nDiv * deltaZ;
-		yi += deltaY;
-	}
-}
-
-std:string box3 (float x, float y, float z, int nDiv){
-
+std::string box (float x, float y, float z, int nDiv){
 	std::string plane, line;
-	std::string ret = "";
-
-	std::istringstream iss;
-
-	/*
-	FRONT FACE
-	*/
-	plane = plane (x, z/2, y, nDiv);
-	iss (plane);
-	std::getline(iss, line);
-	while (std::getline(iss, line)){
-		ret += rotateX(line);
-	}
-
-	/*
-	BACK FACE
-	
-	It's not necessary to recalculate the vertices to the back face, as they are the same for the front face... rotated.
-
-	plane = plane (x, z/2, y, nDiv);
-	*/
-	iss(plane);
-	std::getline(iss, line);
-	while (std::getline(iss, line)){
-		ret += rotate3X(line);
-	}
-
-	/*
-	UP FACE
-	*/
-	plane = plane (x, y/2, z, nDiv);
-	ret += plane;
-
-	/*
-	DOWN FACE
-	
-	plane = plane (x, y/2, z, nDiv);
-	*/
-	iss(plane);
-	std::getline(iss, line);
-	while (std::getline(iss, line)){
-		ret += rotate2X (line);
-	}
-
-	/*
-	RIGHT FACE
-	*/
-	plane = plane (y, x/2, z, nDiv);
-	iss(plane);
-	std::getline(iss, line);
-	while (std::getline(iss, line)){
-		ret += rotate3Z (line);
-	}
-
-	/*
-	LEFT FACE
-
-	plane = plane (y, x/2, z, nDiv);
-	*/
-	iss(plane);
-	std::getline(iss, line);
-	while (std::getline(iss, line)){
-		ret += rotateZ (line);
-	}
-
-	return ret;
-}
-
-
-
-std::string drawBox (float x, float y, float z, int nDiv){
-	std::string ret = "", plane, line;
+	std::ostringstream os;
 	std::istringstream iss;
 	int vertices, count;
 
@@ -386,43 +9,43 @@ std::string drawBox (float x, float y, float z, int nDiv){
 	std::getline(iss1, line);
 	count = stoi(line, nullptr, 10);
 	while (std::getline(iss1, line)){
-		ret += line + "\n";
+		os << line << endl;
 	}
 	plane = drawPlaneXY(x, y, -z/2, nDiv, -1);
 	std::istringstream iss2 (plane);
 	std::getline(iss2, line);
 	count += stoi(line, nullptr, 10);
 	while (std::getline(iss2, line)){
-		ret += line + "\n";
+		os << line << endl;
 	}
 	plane = drawPlaneXZ(x, y/2, z, nDiv, 1);
 	std::istringstream iss3 (plane);
 	std::getline(iss3, line);
 	count += stoi(line, nullptr, 10);
 	while (std::getline(iss3, line)){
-		ret += line + "\n";
+		os << line << endl;
 	}
 	plane = drawPlaneXZ(x, -y/2, z, nDiv, -1);
 	std::istringstream iss4 (plane);
 	std::getline(iss4, line);
 	count += stoi(line, nullptr, 10);
 	while (std::getline(iss4, line)){
-		ret += line + "\n";
+		os << line << endl;
 	}
 	plane = drawPlaneYZ(x/2, y, z, nDiv, 1);
 	std::istringstream iss5 (plane);
 	std::getline(iss5, line);
 	count += stoi(line, nullptr, 10);
 	while (std::getline(iss5, line)){
-		ret += line + "\n";
+		os << line << endl;
 	}
 	plane = drawPlaneYZ(-x/2, y, z, nDiv, -1);
 	std::istringstream iss6 (plane);
 	std::getline(iss6, line);
 	count += stoi(line, nullptr, 10);
 	while (std::getline(iss6, line)){
-		ret += line + "\n";
+		os << line << endl;
 	}
 
-	return std::to_string(count) + "\n" + ret;
+	return std::to_string(count) + "\n" + os.str();
 }
--- phase1/plane.cpp
+++ phase1/plane.cpp
@@ -1,54 +1,6 @@
-//SORRY FOR BAD ENGLISH :/
-
-/*
-	The final version are the three drawPlane__ functions
-	-----------------------------------------------------
-*/
-
-/*
-	This is for the simplest case of the plane
-	When doing the other cases, they can be used for this case and this one can be removed
-*/
-
-void plane (float l, FILE* f){
-
-	/* 
-		l must be greater than 0:
-		validate here or before?
-	*/
-
-	l = l/2;
-
-	/*
-		print the following to f
-
-	vertices:
-	A -> l 0 l
-	B -> l 0 -l
-	C -> -l 0 -l
-	D -> -l 0 l
-	//number of vertices for the triangles (2 triangles each with 3 vertices)
-	6
-	//triangle #1
-	vertex A
-	vertex B
-	vertex D
-	//triangle #2
-	vertex B
-	vertex C
-	vertex D
-	*/
-}
-
-/*
-	Calculates the points to draw a plane paralel to the x0z face.
-	The 'y' argument is to allow to draw a plane out of the x0z face.
-	if 'orient' is 1, the plane will be oriented upwards. if it is -1, will be oriented downwards
-*/
-
-std::string drawPlaneXZ (float x, float y, float z, int nDiv, int orient){
+std::string planeXZ (float x, float y, float z, int nDiv, int orient){
 	
-	std::string ret = "";
+	std::ostringstream os;
 
 	int count = 0;
 
@@ -64,20 +16,24 @@ std::string drawPlaneXZ (float x, float y, float z, int nDiv, int orient){
 	for (int i = 1; i <= nDiv; i++){
 		for (int j = 1; j <= nDiv; j++){
 
-			ret += std::to_string(xi) + " " + std::to_string(y) + " " + std::to_string(zi) + "\n";
-			if (orient == 1)
-				ret += std::to_string(xi) + " " + std::to_string(y) + " " + std::to_string(zi-deltaZ) + "\n";
-			else if (orient == -1)
-				ret += std::to_string(xi-deltaX) + " " + std::to_string(y) + " " + std::to_string(zi) + "\n";
+			if (orient == 1){
+				os << xi << " " << y << " " << zi << endl;				
+				os << xi << " " << y << " " << (zi-deltaZ) << endl;
+				os << (xi-deltaX) << " " << y << " " << (zi-deltaZ) << endl;
 
-			ret += std::to_string(xi-deltaX) + " " + std::to_string(y) + " " + std::to_string(zi-deltaZ) + "\n";
+				os << xi << " " << y << " " << zi << endl;				
+				os << (xi-deltaX) << " " << y << " " << (zi-deltaZ) << endl;
+				os << (xi-deltaX) << " " << y << " " << zi << endl;
+			}
+			else {
+				os << xi << " " << y << " " << zi << endl;				
+				os << (xi-deltaX) << " " << y << " " << zi << endl;
+				os << (xi-deltaX) << " " << y << " " << (zi-deltaZ) << endl;
 
-			ret += std::to_string(xi) + " " + std::to_string(y) + " " + std::to_string(zi) + "\n";
-			ret += std::to_string(xi-deltaX) + " " + std::to_string(y) + " " + std::to_string(zi-deltaZ) + "\n";
-			if (orient == 1)
-				ret += std::to_string(xi-deltaX) + " " + std::to_string(y) + " " + std::to_string(zi) + "\n";
-			else if (orient == -1)
-				ret += std::to_string(xi) + " " + std::to_string(y) + " " + std::to_string(zi-deltaZ) + "\n";
+				os << xi << " " << y << " " << zi << endl;				
+				os << (xi-deltaX) << " " << y << " " << (zi-deltaZ) << endl;
+				os << xi << " " << y << " " << (zi-deltaZ) << endl;
+			}
 
 			count += 6;
 			xi = x - j * deltaX;
@@ -86,12 +42,12 @@ std::string drawPlaneXZ (float x, float y, float z, int nDiv, int orient){
 		zi = z - i * deltaZ;
 		xi = x;
 	}
-	return std::to_string(count) + "\n" + ret;
+	return std::to_string(count) + "\n" + os.str();
 }
 
-std::string drawPlaneXY (float x, float y, float z, int nDiv, int orient){
+std::string planeXY (float x, float y, float z, int nDiv, int orient){
 	
-	std::string ret = "";
+	std::ostringstream os;
 
 	int count = 0;
 
@@ -107,20 +63,24 @@ std::string drawPlaneXY (float x, float y, float z, int nDiv, int orient){
 	for (int i = 1; i <= nDiv; i++){
 		for (int j = 1; j <= nDiv; j++){
 
-			ret += std::to_string(xi) + " " + std::to_string(yi) + " " + std::to_string(z) + "\n";
-			if (orient == 1)
-				ret += std::to_string(xi) + " " + std::to_string(yi+deltaY) + " " + std::to_string(z) + "\n";
-			else if (orient == -1)
-				ret += std::to_string(xi-deltaX) + " " + std::to_string(yi) + " " + std::to_string(z) + "\n";
+			if (orient == 1){
+				os << xi << " " << yi << " " << z << endl;
+				os << xi << " " << (yi+deltaY) << " " << z << endl;
+				os << (xi-deltaX) << " " << (yi+deltaY) << " " << z << endl; 
 
-			ret += std::to_string(xi-deltaX) + " " + std::to_string(yi+deltaY) + " " + std::to_string(z) + "\n";
+				os << xi << " " << yi << " " << z << endl;
+				os << (xi-deltaX) << " " << (yi+deltaY) << " " << z << endl; 				
+				os << (xi-deltaX) << " " << yi << " " << z << endl;
+			}
+			else {
+				os << xi << " " << yi << " " << z << endl;
+				os << (xi-deltaX) << " " << yi << " " << z << endl;	
+				os << (xi-deltaX) << " " << (yi+deltaY) << " " << z << endl; 
 
-			ret += std::to_string(xi) + " " + std::to_string(yi) + " " + std::to_string(z) + "\n";
-			ret += std::to_string(xi-deltaX) + " " + std::to_string(yi+deltaY) + " " + std::to_string(z) + "\n";
-			if (orient == 1)
-				ret += std::to_string(xi-deltaX) + " " + std::to_string(yi) + " " + std::to_string(z) + "\n";
-			else if (orient == -1)
-				ret += std::to_string(xi) + " " + std::to_string(yi+deltaY) + " " + std::to_string(z) + "\n";
+				os << xi << " " << yi << " " << z << endl;
+				os << (xi-deltaX) << " " << (yi+deltaY) << " " << z << endl; 				
+				os << xi << " " << (yi+deltaY) << " " << z << endl;
+			}
 
 			count += 6;
 			xi = x - j * deltaX;
@@ -129,12 +89,12 @@ std::string drawPlaneXY (float x, float y, float z, int nDiv, int orient){
 		yi = -y + i * deltaY;
 		xi = x;
 	}
-	return std::to_string(count) + "\n" + ret;
+	return std::to_string(count) + "\n" + os.str();
 }
 
-std::string drawPlaneYZ (float x, float y, float z, int nDiv, int orient){
+std::string planeYZ (float x, float y, float z, int nDiv, int orient){
 	
-	std::string ret = "";
+	std::ostringstream;
 
 	int count = 0;
 
@@ -150,20 +110,25 @@ std::string drawPlaneYZ (float x, float y, float z, int nDiv, int orient){
 	for (int i = 1; i <= nDiv; i++){
 		for (int j = 1; j <= nDiv; j++){
 
-			ret += std::to_string(x) + " " + std::to_string(yi) + " " + std::to_string(zi) + "\n";
-			if (orient == 1)
-				ret += std::to_string(x) + " " + std::to_string(yi+deltaY) + " " + std::to_string(zi) + "\n";
-			else if (orient == -1)
-				ret += std::to_string(x) + " " + std::to_string(yi) + " " + std::to_string(zi+deltaZ) + "\n";
+			if (orient == 1){
+				os << x << " " << yi << " " << zi << endl;
+				os << x << " " << (yi+deltaY) << " " << zi << endl;
+				os << x << " " << (yi+deltaY) << " " << (zi+deltaZ) << endl;
 
-			ret += std::to_string(x) + " " + std::to_string(yi+deltaY) + " " + std::to_string(zi+deltaZ) + "\n";
+				os << x << " " << yi << " " << zi << endl;
+				os << x << " " << (yi+deltaY) << " " << (zi+deltaZ) << endl;
+				os << x << " " << yi << " " << (zi+deltaZ) << endl;
+			}
+			else {
+				os << x << " " << yi << " " << zi << endl;
+				os << x << " " << yi << " " << (zi+deltaZ) << endl;
+				os << x << " " << (yi+deltaY) << " " << (zi+deltaZ) << endl;
+				
+				os << x << " " << yi << " " << zi << endl;
+				os << x << " " << (yi+deltaY) << " " << (zi+deltaZ) << endl;
+				os << x << " " << (yi+deltaY) << " " << zi << endl;
 
-			ret += std::to_string(x) + " " + std::to_string(yi) + " " + std::to_string(zi) + "\n";
-			ret += std::to_string(x) + " " + std::to_string(yi+deltaY) + " " + std::to_string(zi+deltaZ) + "\n";
-			if (orient == 1)
-				ret += std::to_string(x) + " " + std::to_string(yi) + " " + std::to_string(zi+deltaZ) + "\n";
-			else if (orient == -1)
-				ret += std::to_string(x) + " " + std::to_string(yi+deltaY) + " " + std::to_string(zi) + "\n";
+			}
 
 			count += 6;
 			zi = -z + j * deltaZ;
@@ -172,66 +137,5 @@ std::string drawPlaneYZ (float x, float y, float z, int nDiv, int orient){
 		yi = -y + i * deltaY;
 		zi = -z;
 	}
-	return std::to_string(count) + "\n" + ret;
+	return std::to_string(count) + "\n" + os.str();
 }
-
-/*RIGHT HAND RULE
-
-	X -> 90º rotation
-	2X -> 180º rotation
-	3X -> 270º/-90º rotation
-
-std::string rotateX (std::string vertex){
-	float x, y, z;
-	x = ...;
-	y = ...;
-	z = ...;
-	float aux = y;
-	y = -z;
-	z = aux;
-	return std::to_string(x) + " " + std::to_string(y) + " " + std::to_string(z) + "\n";
-}
-
-std::string rotate2X (std::string vertex){
-	float x, y, z;
-	x = ...;
-	y = ...;
-	z = ...;
-	y = -y;
-	z = -z;
-	return std::to_string(x) + " " + std::to_string(y) + " " + std::to_string(z) + "\n";
-}
-
-std::string rotate3X (std::string vertex){
-	float x, y, z;
-	x = ...;
-	y = ...;
-	z = ...;
-	float aux = y;
-	y = z;
-	z = -aux;
-	return std::to_string(x) + " " + std::to_string(y) + " " + std::to_string(z) + "\n";
-}
-
-std::string rotateZ (std::string vertex){
-	float x, y, z;
-	x = ...;
-	y = ...;
-	z = ...;
-	float aux = x;
-	x = -y;
-	y = aux;
-	return std::to_string(x) + " " + std::to_string(y) + " " + std::to_string(z) + "\n";
-}
-
-std::string rotate3Z (std::string vertex){
-	float x, y, z;
-	x = ...;
-	y = ...;
-	z = ...;
-	float aux = x;
-	x = y;
-	y = -aux;
-	return std::to_string(x) + " " + std::to_string(y) + " " + std::to_string(z) + "\n";
-}
-*/
