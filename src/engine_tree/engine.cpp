1d1fbef5c340e0392b41d8e6c14e2e91c1754603
João Pereira <joaopereira_19@hotmail.com>
2017-03-31 17:37:18 +0100

    maximized window by default

--- src/engine_tree/engine.cpp
+++ src/engine_tree/engine.cpp
@@ -475,7 +475,7 @@ int main(int argc, char **argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
 	glutInitWindowPosition(100,100);
-	glutInitWindowSize(800,800);
+	glutInitWindowSize(glutGet(GLUT_SCREEN_WIDTH), glutGet(GLUT_SCREEN_HEIGHT));
 	glutCreateWindow("Pratical Assignment");
 
 	// Required callback registry 
